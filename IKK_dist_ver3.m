724cc84952a234cd4320c30ec6f495544389b7dd
bsdfriedman <bsdfriedman@yahoo.com>
2018-02-22 11:27:25 -0500

    IKK_dist rg_separate

--- IKK_dist_ver3.m
+++ IKK_dist_ver3.m
@@ -21,14 +21,36 @@ spot_position.rg_same = sum(gg_dist == rr_dist)/size(rr_dist,1);
 
 % calculate the kk dist
 if strcmp(stationary_protein,'RFP') == 1
-    spot_position.kk_hist = (rr_dist - gg_dist)/2;
+    kk_hist = (rr_dist - gg_dist)/2;
 elseif strcmp(stationary_protein,'GFP') == 1
-    spot_position.kk_hist = (gg_dist - rr_dist)/2;
+    kk_hist = (gg_dist - rr_dist)/2;
 else
     error('stationary_protein must either be "RFP" or "GFP"');
 end
+
+% calculate distances between cells separately depending on which spot is outside
+if strcmp(stationary_protein,'RFP') == 1
+    kk_dist_r_out_mean = mean(abs(kk_hist(kk_hist>0)));
+    kk_dist_g_out_mean = mean(abs(kk_hist(kk_hist<0)));
+    kk_dist_r_out_std = std(abs(kk_hist(kk_hist>0)));
+    kk_dist_g_out_std = std(abs(kk_hist(kk_hist<0)));
+elseif strcmp(stationary_protein,'GFP') == 1
+    kk_dist_r_out_mean = mean(abs(kk_hist(kk_hist<0)));
+    kk_dist_g_out_mean = mean(abs(kk_hist(kk_hist>0)));
+    kk_dist_r_out_std = std(abs(kk_hist(kk_hist<0)));
+    kk_dist_g_out_std = std(abs(kk_hist(kk_hist>0)));
+else
+    error('stationary_protein must either be "RFP" or "GFP"');
+end
+
 % calculate the distances
+spot_position.kk_hist = kk_hist;
 spot_position.kk_xpos_mean = mean(spot_position.kk_hist);
 spot_position.kk_xpos_std = std(spot_position.kk_hist);
 spot_position.kk_dist_mean = mean(abs(spot_position.kk_hist));
 spot_position.kk_dist_std = std(abs(spot_position.kk_hist));
+spot_position.kk_dist_r_out_mean = kk_dist_r_out_mean;
+spot_position.kk_dist_r_out_std = kk_dist_r_out_std;
+spot_position.kk_dist_g_out_mean = kk_dist_g_out_mean;
+spot_position.kk_dist_g_out_std = kk_dist_g_out_std;
+
